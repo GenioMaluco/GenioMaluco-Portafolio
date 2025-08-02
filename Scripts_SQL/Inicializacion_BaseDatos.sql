bed737b2cbe7005a86138ce09f339be158bc9f87
GenioMaluco <adrian.arayaaguilar@gmail.com>
2025-08-02 10:58:16 -0600

    Primer Commit del Proyecto Inicializacion de repositorio

---
+++ Scripts_SQL/Inicializacion_BaseDatos.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Habilidades (
+    id INT PRIMARY KEY IDENTITY,
+    tipo VARCHAR(20) CHECK (tipo IN ('blanda', 'tecnica', 'herramienta', 'lenguaje')),
+    nombre VARCHAR(50) NOT NULL,
+    nivel TINYINT,
+    categoria VARCHAR(30) NULL,
+    icono VARCHAR(100) NULL
+);
+
+CREATE TABLE Proyectos (
+    id INT PRIMARY KEY IDENTITY,
+    titulo VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    imagen_url VARCHAR(255),
+    url VARCHAR(255),
+    tags VARCHAR(255)
+);
